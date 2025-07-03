86b8086e3f2b05a0430ec3e9da5e83df1a47ad1b
jinhyo <kjhyo0220@gmail.com>
2025-07-03 22:43:11 +0900

    ♻️ Refactor: tip, 공지사항 페이지 라우팅 type 추가 (#1)
    type에 따라 분기 처리되도록 props 추가

--- src/pages/PromptGuidePage/PromptGuideDetailPage.tsx
+++ src/pages/PromptGuidePage/PromptGuideDetailPage.tsx
@@ -1,5 +1,21 @@
-const PromptGuideDetailPage = () => {
-  return <div>프롬프트 TIP - 게시물 페이지, 공지사항 - 게시물 페이지</div>;
+/**
+ * TODO:
+ * - type에 따라 TIP/공지사항 렌더링 분기 처리 필요
+ *
+ * @author 김진효
+ * **/
+
+interface PromptGuideDetailPageProps {
+  type: 'tip' | 'notice';
+}
+
+const PromptGuideDetailPage = ({ type }: PromptGuideDetailPageProps) => {
+  return (
+    <>
+      <div>프롬프트 TIP - 게시물 페이지, 공지사항 - 게시물 페이지</div>
+      <p>{type}상세 페이지</p>
+    </>
+  );
 };
 
 export default PromptGuideDetailPage;
--- src/pages/PromptGuidePage/PromptGuidePage.tsx
+++ src/pages/PromptGuidePage/PromptGuidePage.tsx
@@ -1,5 +1,21 @@
-const PromptGuidePage = () => {
-  return <div>프롬프트 TIP & 프롬프트 TIP-공지사항 페이지</div>;
+/**
+ * TODO:
+ * - type에 따라 TIP/공지사항 렌더링 분기 처리 필요
+ *
+ * @author 김진효
+ * **/
+
+interface PromptGuidePageProps {
+  type: 'tip' | 'notice';
+}
+
+const PromptGuidePage = ({ type }: PromptGuidePageProps) => {
+  return (
+    <>
+      <div>프롬프트 TIP & 프롬프트 TIP-공지사항 페이지</div>
+      <p>{type}페이지</p>
+    </>
+  );
 };
 
 export default PromptGuidePage;
