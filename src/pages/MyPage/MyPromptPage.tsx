cda11709e99f167aede814029409797b125740ff
fbehdgus <danny90697@gmail.com>
2025-07-11 03:39:12 +0900

    ♻️ Refactor: 내 프롬프트 페이지 UI Layout 수정(#16)

--- src/pages/MyPage/MyPromptPage.tsx
+++ src/pages/MyPage/MyPromptPage.tsx
@@ -4,14 +4,25 @@ import type { Prompt } from './components/PromptCard';
 import { useState, useEffect } from 'react';
 import BlueArchiveIcon from '@/assets/icon-archive-blue.svg'
 
-
+/**
+ * TODO:
+ * - 스크롤 바 구현 필요
+ * - 리뷰 작성하기 버튼 클릭시 함수 추후 작성 예정
+ * - api 연동과 관련된 함수 추후 작성 예정
+ * @author 류동현
+ * **/
 
 //더미데이터
-const DUMMY_LIKED_PROMPTS: Prompt[] = [
-  { id: 101, title: '강아지 그림 그려주는 프롬프트', model: 'DALL-E',tags:['개발','파이썬','코딩'],author:'홍길동' },
-  { id: 102, title: '여행 계획 짜주는 프롬프트', model: 'ChatGPT-4o',tags:['개발','파이썬','코딩'],author:'매튜' },
-  { id: 103, title: '블로그 글 초안 작성 프롬프트', model: 'Claude',tags:['개발','파이썬','코딩'],author:'오타니' },
-];
+  const DUMMY_LIKED_PROMPTS: Prompt[] = [
+    { id: 101, title: '강아지 그림 그려주는 프롬프트', model: 'DALL-E',tags:['개발','파이썬','코딩'],author:'홍길동' },
+    { id: 102, title: '여행 계획 짜주는 프롬프트', model: 'ChatGPT-4o',tags:['개발','파이썬','코딩'],author:'매튜' },
+    { id: 103, title: '파이썬으로 5분만에 구슬깨기 게임 만들어주는 프롬프트', model: 'Claude',tags:['개발','파이썬','코딩'],author:'오타니' },
+  ];
+  const DUMMY_LIKED_PROMPTS2: Prompt[] = [
+    { id: 101, title: '강아지 그림 그려주는 프롬프트', model: 'DALL-E',tags:['개발','파이썬','코딩'],author:'홍길동' },
+    { id: 102, title: '여행 계획 짜주는 프롬프트', model: 'ChatGPT-4o',tags:['개발','파이썬','코딩'],author:'매튜' },
+    { id: 103, title: '파이썬으로 5분만에 구슬깨기 게임 만들어주는 프롬프트', model: 'Claude',tags:['개발','파이썬','코딩'],author:'오타니' },
+  ];
 
 
 
@@ -25,16 +36,18 @@ const MyPromptPage = () => {
 
   const [activeTab, setActiveTab] = useState<'authored' | 'downloaded' | 'liked'>('authored'); // 'authored', 'downloaded', 'liked'
   const [prompts, setPrompts] = useState(DUMMY_LIKED_PROMPTS);
+ 
 
   useEffect(() => {
     // 탭이 바뀔 때마다 해당 데이터를 API로 호출하는 로직
     // const fetchedData = await api.getPrompts(activeTab);
     // setPrompts(fetchedData);
+    setPrompts(DUMMY_LIKED_PROMPTS2);
   }, [activeTab]);
 
 
   return (
-    <div className="flex items-center justify-center min-h-screen ">
+    <div className="flex items-center justify-center min-h-screen bg-background">
     <div className=" w-full max-w-[1236px] mx-[102px] ">
       <div className="flex items-center gap-[10px] mb-[56px]">
            <img src={BlueArchiveIcon} alt="BlueArchiveIcon" className="w-[24px] h-[24px]" />
@@ -42,9 +55,9 @@ const MyPromptPage = () => {
       </div>
       <MyPromptsTabs activeTab={activeTab} setActiveTab={setActiveTab} />
       
-      <div className="mt-4">
+      <div className="bg-white">
         {prompts.map(prompt => (
-          <PromptCard key={prompt.id} type={activeTab} promptData={prompt} />
+          <PromptCard key={prompt.id} type={activeTab} promptData={prompt} DeletePrompt={()=>{}} EditPrompt={()=>{}} DeleteLike={()=>{}} />
         ))}
       </div>
     </div>
