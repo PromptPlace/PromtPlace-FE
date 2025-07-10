97872b543369e7d2f5eb8558b2ff02d07823e60f
fbehdgus <danny90697@gmail.com>
2025-07-11 03:39:12 +0900

    ♻️ Refactor: 내 프롬프트 페이지 UI Layout 수정(#16)

--- src/pages/MyPage/components/PromptCard.tsx
+++ src/pages/MyPage/components/PromptCard.tsx
@@ -1,5 +1,7 @@
 import BigBlueHeart from '@/assets/icon-heart-blue-big.svg'
 import PrimaryButton from '@components/Button/PrimaryButton';
+import kebabMenu from '@/assets/icon-kebabMenu.svg'
+import { useState } from 'react';
 
 export interface Prompt {
   id: number;
@@ -15,24 +17,28 @@ type TabType = 'authored' | 'downloaded' | 'liked';
 interface PromptCardProps {
   type: TabType;
   promptData: Prompt;
+  DeletePrompt:(id:number)=>void;
+  EditPrompt:(id:number)=>void;
+  DeleteLike:(id:number)=>void;
 }
 
 
+export const PromptCard = ({ type, promptData, DeletePrompt, EditPrompt, DeleteLike} : PromptCardProps ) => {
+        const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-export const PromptCard = ({ type, promptData} : PromptCardProps ) => {
   return (
-    <div className="flex items-center justify-between  border-b">
+    <div className="flex items-center   border-b-[1px] border-b-white-stroke w-full  py-[10px] h-[92px]">
       {/* 왼쪽 영역: 제목과 모델 이름 */}
       
-        <div className=" text-text-on-white text-[22px] py-[22px] font-bold w-[635px]">{promptData.title}</div>
-        <div className="text-text-on-background text-[20px] font-medium py-[23.5px] w-[223px]">{promptData.model}</div>
+        <div className="flex items-center justify-center text-text-on-white text-[22px]  font-bold w-[635px]">{promptData.title}</div>
+        <div className="flex items-center justify-center text-text-on-background text-[20px] font-medium  w-[223px]">{promptData.model}</div>
     
 
       {/* 오른쪽 영역: type에 따라 내용이 바뀜 */}
-      <div className="flex items-center gap-6">
+     
         {/* 태그 (다운로드 탭에서는 보이지 않음) */}
         {(type === 'authored' || type === 'liked') && (
-          <div className="text-text-on-background text-[20px] font-medium py-[23.5px] w-[263px]">
+          <div className="flex items-center justify-center text-text-on-background text-[20px] font-medium py-[23.5px] w-[263px]">
             {promptData.tags?.map(tag => `#${tag} `)}
           </div>
         )}
@@ -42,20 +48,42 @@ export const PromptCard = ({ type, promptData} : PromptCardProps ) => {
 
         {/* 버튼 영역 (type별로 다른 버튼) */}
         {type === 'authored' && (
-          <button>...</button> // 케밥 메뉴(점 3개) 아이콘
+          <div className="flex items-center justify-center h-[72px]  w-[115px] py-[10px]  ">
+            <div className="relative">
+          <button onClick={() => setIsDropdownOpen(!isDropdownOpen)} className={`flex items-center justify-center h-[28px] w-[28px] rounded-[50px]  ${isDropdownOpen ? 'bg-secondary-pressed' : 'bg-transparent'}`}><img src={kebabMenu} alt="케밥 메뉴"  /></button> 
+          {isDropdownOpen && (
+            <div className="absolute  top-full mt-[11px] w-[91px] bg-white rounded-md shadow-lg z-10">
+              <button 
+                onClick={() => {DeletePrompt(promptData.id); setIsDropdownOpen(false)}} 
+                className="block w-full h-[36px] text-[16px] border-b-[1px] border-b-white-stroke text-text-on-background bg-secondary hover:bg-secondary-pressed"
+              >
+                삭제하기
+              </button>
+              <button 
+                onClick={() => {EditPrompt(promptData.id); setIsDropdownOpen(false)}} 
+                className="block w-full h-[36px] text-[16px] text-text-on-background bg-secondary hover:bg-secondary-pressed"
+              >
+                수정하기
+              </button>
+            </div>
+            
+          )}
+          </div>
+          </div>
         )}
         {type === 'downloaded' && (
-          
-          <PrimaryButton buttonType="review" text="리뷰 작성하기" onClick={() => {}} />    //버튼 공통 컴포넌트 수정 추후 상의 필요
+          <div className="flex items-center justify-center h-[72px]  w-[198px]">
+          <PrimaryButton buttonType="review" text="리뷰 작성하기" onClick={() => {}} />   
+          </div>
         )}
         {type === 'downloaded' && (
-          <div className="text-text-on-white text-[20px] font-medium py-[23.5px] w-[180px] px-[44px]">{promptData.author}</div>
+          <div className="flex items-center justify-center text-text-on-white text-[20px] font-medium py-[23.5px] w-[180px] px-[44px]">{promptData.author}</div>
         )}
 
         {type === 'liked' && (
-          <button><img src={BigBlueHeart} alt="좋아요 큰하트" className=" h-[22px] py-[22px] w-[115px]" /></button> // 하트 아이콘
+          <button onClick={() => DeleteLike(promptData.id)}><img src={BigBlueHeart} alt="좋아요 큰하트" className=" h-[22px]  w-[115px]" /></button> // 하트 아이콘
         )}
-      </div>
+        
     </div>
   );
 };
