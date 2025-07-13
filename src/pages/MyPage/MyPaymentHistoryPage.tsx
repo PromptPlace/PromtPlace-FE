c99ea7467275f002933440566a62bda9af1e1008
fbehdgus <danny90697@gmail.com>
2025-07-13 22:03:48 +0900

    ✨ Feat: 회원정보 페이지 UI 구현(#18)

--- src/pages/MyPage/MyPaymentHistoryPage.tsx
+++ src/pages/MyPage/MyPaymentHistoryPage.tsx
@@ -16,17 +16,46 @@ const DUMMY_PAYMENT_HISTORY = [
   {
     prompt_id: 2,
     purchase_at: '2025-07-04T12:30:00.000Z',
-    title: '프롬프트 구매',
+    title: '효율적인 경쟁사 분석 프롬프트',
     amount: 20000,
     paymentMethod: '계좌이체',
   },
   {
     prompt_id: 3,
     purchase_at: '2025-07-03T17:45:00.000Z',
-    title: '프롬프트 구매',
+    title: '정부지원사업 무조건 선정되는 사업계획서 프롬프트',
     amount: 30000,
     paymentMethod: '신용카드',
   },
+  {
+    prompt_id: 4,
+    purchase_at: '2025-07-02T10:15:00.000Z',
+    title: '프롬프트 구매',
+    amount: 40000,
+    paymentMethod: '계좌이체',
+  },
+  {
+    prompt_id: 5,
+    purchase_at: '2025-07-01T08:30:00.000Z',
+    title: '프롬프트 구매',
+    amount: 50000,
+    paymentMethod: '신용카드',
+  },
+  {
+    prompt_id: 6,
+    purchase_at: '2025-06-30T14:00:00.000Z',
+    title: '프롬프트 구매',
+    amount: 60000,
+    paymentMethod: '계좌이체',
+  },
+  {
+    prompt_id: 7,
+    purchase_at: '2025-06-29T11:20:00.000Z',
+    title: '프롬프트 구매',
+    amount: 70000,
+    paymentMethod: '신용카드',
+  },
+  
 ];
 
 const PaymentHistoryPage = () => {
@@ -38,9 +67,10 @@ const PaymentHistoryPage = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-background ">
-      <div className="w-full max-w-[1236px] ">
+    <div className="flex justify-center h-screen bg-background ">
 
+      <div className="flex flex-col w-full max-w-[1236px] pt-[92px] h-full">
+        <div className="shrink-0">
         <div className="flex items-center gap-[10px] mb-[27px]">
           <img src={iconPerson} alt="person icon" className="w-[32px] h-[32px]" />
           <div className="text-[32px] text-primary-hover font-bold">회원정보</div>
@@ -54,10 +84,10 @@ const PaymentHistoryPage = () => {
             <span className="text-[24px] text-primary-hover font-bold">결제 내역</span>
         </div>
         </div>
-
-      <div className="bg-white">
+        </div>
+      <div className="overflow-y-auto flex-grow max-h-[580px]">
         {paymentHistory.map(item => (
-          <PaymentHistoryRow key={item.id} transaction={item} />
+          <PaymentHistoryRow key={item.prompt_id} transaction={item} />
         ))}
       </div>
       </div>
--- src/pages/MyPage/components/PaymentHistoryRow.tsx
+++ src/pages/MyPage/components/PaymentHistoryRow.tsx
@@ -1,5 +1,4 @@
-import { Link } from 'react-router-dom';
-import iconArrow from '@assets/icon-arrow-left-black.svg'
+import CardHeader from './CardHeader';
 // 이 컴포넌트가 받을 데이터의 타입을 정의
 interface Transaction {
     prompt_id: number;
@@ -18,15 +17,15 @@ interface Transaction {
         const displayDate = new Date(transaction.purchase_at).toLocaleString();
     
     return (
-      <div className="flex items-center py-[10px] border-b-[1px] border-white-stroke h-[116px]">
+      <div className="flex items-center py-[10px] border-b-[1px] border-white-stroke h-[116px] bg-white">
         <div className="flex flex-col w-[946px] pl-[40px] gap-[10px]">
-          <span className="text-[14px] text-text-on-background">{displayDate}</span>
-          <span className="w-[247px]">
-          <Link to={`/guide/notice/${transaction.prompt_id}`} className="text-[22px] font-bold text-text-on-white  flex items-center gap-[10px]">
-            {transaction.title}
-            <img src={iconArrow} alt="arrow" className="w-[16px] h-[16px]  rotate-180" />
-          </Link>
-          </span>
+          <CardHeader
+            date={displayDate}
+            dateFormat="dateOnly"
+            title={transaction.title}
+            linkUrl={`/guide/notice/${transaction.prompt_id}`}
+          />
+         
         </div>
         
           <div className="flex items-center justify-center w-[145px] text-[20px] text-text-on-white font-medium">{transaction.amount.toLocaleString()}원</div>
