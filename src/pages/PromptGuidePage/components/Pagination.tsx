b38db052a0c496815d9b8df844ac2ed938652898
luii0415 <becky45210193@gmail.com>
2025-07-20 23:18:58 +0900

    Design : 프롬프트 팁 & 공지사항 페이지 관련 UI 수정 (#39)
    프롬프트 팁 & 공지사항 페이지, 팁 & 공지사항 상세 페이지 UI 수정 및 코드 정리

--- src/pages/PromptGuidePage/components/Pagination.tsx
+++ src/pages/PromptGuidePage/components/Pagination.tsx
@@ -9,41 +9,45 @@ interface Postlist {
 // 페이지네이션 테이블
 export function PromptTableList({ data, onRowClick }: { data: Postlist[]; onRowClick: (id: number) => void }) {
   return (
-    <table className="w-[1236px] h-[530px] mt-[53px] mb-[104px] mx-[102px]">
-      <thead>
-        <tr className="h-[90px] border-b-[1px] border-[var(--color-text-on-white)] pt-[10px] pb-[10px] bg-[var(--color-background)] font-bold text-[24px] text-[var(--color-text-on-white)]">
-          <th className="w-[750px] h-[70px] px-[10px] py-[20px] ">제목</th>
-          <th className="w-[223px] h-[70px] px-[10px] py-[20px] ">첨부</th>
-          <th className="w-[263px] h-[70px] px-[10px] py-[20px] ">등록일</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.length === 0 ? (
-          <tr>
-            <td colSpan={3} className="text-center py-4 text-gray-400">
-              게시글이 없습니다.
-            </td>
+    <div className="flex justify-center">
+      <table className="w-full max-w-[1236px] h-[530px] mt-[53px] mb-[104px] mx-[102px] cursor-pointer">
+        <thead>
+          <tr className="h-[90px] border-b-[1px] border-text-on-white pt-[10px] pb-[10px] bg-background font-bold text-[24px] text-text-on-white">
+            <th className="w-[750px] h-[70px] px-[10px] py-[20px] ">제목</th>
+            <th className="w-[223px] h-[70px] px-[10px] py-[20px] ">첨부</th>
+            <th className="w-[263px] h-[70px] px-[10px] py-[20px] ">등록일</th>
           </tr>
-        ) : (
-          data.map((post) => (
-            <tr
-              key={post.id}
-              className="h-[88px] py-[10px] border-b-[1px]  border-[var(--color-white-stroke)] bg-[var(--color-white)] hover:bg-[var(--color-secondary)] focus:bg-[var(--color-secondary-pressed)]"
-              onClick={() => onRowClick(post.id)}>
-              <td className="w-[750px] h-[65px] px-[80px] py-[20px] text-left font-medium text-[20px] text-[var(--color-text-on-white)]">
-                {post.title}
-              </td>
-              <td className=" w-[223px] h-[65px] px-[10px] py-[20px]">
-                <div className="flex justify-center items-center">{post.file_url ? <BsPaperclip size={28} /> : ''}</div>
-              </td>
-              <td className="w-[263px] h-[65px] text-center font-medium text-[20px] text-[var(--color-text-on-background)]">
-                {post.create_at}
+        </thead>
+        <tbody>
+          {data.length === 0 ? (
+            <tr>
+              <td colSpan={3} className="text-center py-4 text-gray-400">
+                게시글이 없습니다.
               </td>
             </tr>
-          ))
-        )}
-      </tbody>
-    </table>
+          ) : (
+            data.map((post) => (
+              <tr
+                key={post.id}
+                className="h-[88px] py-[10px] border-b-[1px]  border-white-stroke bg-white hover:bg-secondary focus:bg-secondary-pressed"
+                onClick={() => onRowClick(post.id)}>
+                <td className="w-[750px] h-[65px] px-[80px] py-[20px] text-left font-medium text-[20px] text-text-on-white">
+                  {post.title}
+                </td>
+                <td className=" w-[223px] h-[65px] px-[10px] py-[20px]">
+                  <div className="flex justify-center items-center">
+                    {post.file_url ? <BsPaperclip size={28} /> : ''}
+                  </div>
+                </td>
+                <td className="w-[263px] h-[65px] text-center font-medium text-[20px] text-text-on-background">
+                  {post.create_at}
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
@@ -65,8 +69,8 @@ export function Pagination({
         onClick={() => onPageChange(i)}
         className={`w-[50px] h-[50px] rounded-[50px] p-[10px] ${
           currentPage === i
-            ? 'bg-[var(--color-primary-hover)] font-medium text-[20px] text-[var(--color-white)]'
-            : 'font-medium text-[20px] text-[var(--color-text-on-background)] hover:bg-[#F0F7FF]'
+            ? 'bg-primary-hover font-medium text-[20px] text-white'
+            : 'font-medium text-[20px] text-text-on-background hover:bg-background'
         }`}
         disabled={currentPage === i}>
         {i}
@@ -74,16 +78,18 @@ export function Pagination({
     );
   }
   return (
-    <nav className="flex items-center justify-center h-[50px] mx-[545px] mb-[60px]">
+    <nav className="flex items-center justify-center h-[50px] mx-[545px] mb-[60px] text-text-on-background">
       <button
-        className="w-[50px] h-[50px] px-[6px] py-[8px]"
+        className="w-[50px] h-[50px] px-[6px] py-[8px] rounded-[50px] flex items-center 
+        justify-center hover:bg-secondary active:bg-primary-hover active:text-white"
         onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
         disabled={currentPage === 1}>
         <LuChevronLeft />
       </button>
       {pageButtons}
       <button
-        className="w-[50px] h-[50px] px-[6px] py-[8px]"
+        className="w-[50px] h-[50px] px-[6px] py-[8px] rounded-[50px] flex items-center 
+        justify-center hover:bg-secondary active:bg-primary-hover active:text-white"
         onClick={() => currentPage < totalPages && onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}>
         <LuChevronRight />
