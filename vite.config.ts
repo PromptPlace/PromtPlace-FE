f63473e2bf404d7876b14623bd8ab860a138e0b3
fbehdgus <danny90697@gmail.com>
2025-07-21 20:18:25 +0900

    ✨ Feat: PWA 자동화 설정으로 전환 및 파일 구조 정리(#45)

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,37 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss(), svgr(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png', 'favicon-96x96.png','web-app-manifest-192x192.png', 'web-app-manifest-512x512.png'],
+      manifest: {
+        name: 'PromptPlace',
+        short_name: 'PromptPlace',
+        start_url: '/',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: "standalone",
+        icons: [
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: "any maskable"
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: "any maskable"
+          }
+        ]
+      }
+    })],
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
