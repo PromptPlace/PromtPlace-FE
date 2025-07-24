0231ab52ae68329d2b904d569e20af6ca039e334
fbehdgus <danny90697@gmail.com>
2025-07-25 05:09:27 +0900

    ♻️ Refactor: console 출력 코드 제거(#53)

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App.tsx';
 import { registerSW } from 'virtual:pwa-register';
 
-console.log("VITE_DEV_MODE:", import.meta.env.VITE_DEV_MODE);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,8 +13,4 @@ createRoot(document.getElementById('root')!).render(
 
 if(import.meta.env.VITE_DEV_MODE==='Production'){
   registerSW()
-  console.log("✅ Production mode detected. Registering service worker...");
-}else{
-  console.log("❌ Not in production mode. Skipping service worker registration.");
 }
-
