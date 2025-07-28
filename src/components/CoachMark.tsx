3130a1b7bbbf5786a848d1032e2c784c8fb87916
jinhyo <kjhyo0220@gmail.com>
2025-07-28 20:26:27 +0900

    💄 Design: 코치마크 디자인 수정 (#65)
    relative, absolute 구조 수정

--- src/components/CoachMark.tsx
+++ src/components/CoachMark.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useRef } from 'react';
 
+import { useAuth } from '@/context/AuthContext';
+
+import PrimaryButton from '@components/Button/PrimaryButton';
+import GradientButton from '@components/Button/GradientButton';
+
 import ArrowCreate from '@assets/coachmark/icon-arrow-create.svg';
 import ArrowFilter from '@assets/coachmark/icon-arrow-filter.svg';
 import ArrowFollow from '@assets/coachmark/icon-arrow-follow.svg';
@@ -10,15 +15,9 @@ import ArrowTip from '@assets/coachmark/icon-arrow-tip.svg';
 import Cursor from '@assets/coachmark/icon-cursor.svg';
 import Line from '@assets/coachmark/icon-line.svg';
 import Mouse from '@assets/coachmark/icon-mouse.svg';
-import { HiMagnifyingGlass } from 'react-icons/hi2';
+import arrowDown from '@/assets/icon-arrow-down.svg';
 import ProfileIcon from '@assets/icon-profile-blue-small.svg';
-import UserProfileIcon from '@assets/img-example-profile2.jpg';
-
-import PrimaryButton from '@components/Button/PrimaryButton';
-import GradientButton from '@components/Button/GradientButton';
-
-import { useAuth } from '@/context/AuthContext';
-import FilterBar from '@pages/MainPage/components/filterBar';
+import { HiMagnifyingGlass } from 'react-icons/hi2';
 
 interface CoachMarkProps {
   setShowCoachMark: (state: boolean) => void;
@@ -48,16 +47,15 @@ const CoachMark = ({ setShowCoachMark }: CoachMarkProps) => {
 
         <div className="flex items-center relative ml-[61.26px] w-full max-lg:m-[0] max-w-[1000px]">
           <input
+            disabled
             placeholder="내가 원하는 프롬프트 찾기"
             className="relative flex-1 placeholder:font-SpoqaHanSansNeo placeholder:color-text-on-background placeholder:text-base placeholder:font-normal placeholder:leading-[26px] placeholder:tracking-[0.46px] bg-background rounded-[40px] border border-[#ccc] py-[10px] px-[20px] outline-none focus:border focus:border-primary focus:inset-shadow-inner"
           />
           <div className="absolute left-[-15px] -translate-x-full flex gap-[14px]">
             <div className="text-white text-[18px] font-normal leading-[23px] mt-[23px]">
-              <p>
-                원하는 프롬프트 또는
-                <br />
-                키워드 <span className="text-secondary-pressed">검색하기</span>
-              </p>
+              원하는 프롬프트 또는
+              <br />
+              키워드 <span className="text-secondary-pressed">검색하기</span>
             </div>
 
             <img src={ArrowInput} alt="arrow-input" />
@@ -65,36 +63,36 @@ const CoachMark = ({ setShowCoachMark }: CoachMarkProps) => {
           <HiMagnifyingGlass className="absolute right-[20px] text-text-on-background cursor-pointer" />
         </div>
 
-        <PrimaryButton buttonType="tip" text="프롬프트 TIP" onClick={() => {}} />
-        <div className="absolute top-[77px] right-[418px] flex gap-[14px]">
-          <div className="text-white text-[18px] font-normal leading-[23px] mt-[21px]">
-            <p>
-              프롬프트 작성 <span className="text-secondary-pressed">꿀팁</span>과
-            </p>
-            <p className="text-secondary-pressed">공지사항</p>
-          </div>
+        <div className="relative w-[127px] shrink-0">
+          <PrimaryButton buttonType="tip" text="프롬프트 TIP" onClick={() => {}} />
 
-          <div>
-            <img src={ArrowTip} alt="arrow-tip" />
+          <div className="absolute top-[65px] right-[77px] flex gap-[14px]">
+            <div className="text-white text-[18px] font-normal leading-[23px] mt-[21px] w-full whitespace-nowrap">
+              프롬프트 작성 <span className="text-secondary-pressed">꿀팁</span>과
+              <p className="text-secondary-pressed">공지사항</p>
+            </div>
+
+            <div className="w-[44.24px] h-[35.39px] shrink-0">
+              <img src={ArrowTip} alt="arrow-tip" className="w-full h-full object-contain" />
+            </div>
           </div>
         </div>
 
         {!accessToken && (
-          <>
+          <div className="relative w-[171px] shrink-0">
             <PrimaryButton buttonType="login" text="로그인 / 회원가입" onClick={() => {}} />
-            <div className="absolute top-[77px] right-[219px] flex flex-col">
-              <div className="flex justify-end">
-                <img src={ArrowLogin} alt="arrow-login" />
+            <div className="absolute top-[65px] right-[85.6px] flex flex-col">
+              <div className="flex justify-end w-[33px] h-[48px] shrink-0 w-full">
+                <img src={ArrowLogin} alt="arrow-login w-full h-full object-contain" />
               </div>
-              <div className="text-white text-[18px] font-normal leading-[23px]">
-                <p>
-                  더 많은 서비스를
-                  <br />
-                  누릴 수 있는 <span className="text-secondary-pressed">로그인 기능</span>
-                </p>
+
+              <div className="text-white text-[18px] font-normal leading-[23px] whitespace-nowrap">
+                더 많은 서비스를
+                <br />
+                누릴 수 있는 <span className="text-secondary-pressed">로그인 기능</span>
               </div>
             </div>
-          </>
+          </div>
         )}
 
         <div className="cursor-pointer w-[60px] rounded-full overflow-hidden shrink-0 max-lg:hidden">
@@ -106,39 +104,79 @@ const CoachMark = ({ setShowCoachMark }: CoachMarkProps) => {
                   <img src={ArrowProfile} alt="arrow-profile" />
                 </div>
                 <div className="text-white text-[18px] font-normal leading-[23px]">
-                  <p>
-                    내 프롬프트와 리뷰 등을
-                    <br />
-                    손쉽게 관리할 수 있는
-                    <br />
-                    <span className="text-secondary-pressed">마이페이지 탭</span>
-                  </p>
+                  내 프롬프트와 리뷰 등을
+                  <br />
+                  손쉽게 관리할 수 있는
+                  <br />
+                  <span className="text-secondary-pressed">마이페이지 탭</span>
                 </div>
               </div>
             </div>
           )}
-          {accessToken && (
-            <img
-              src={UserProfileIcon ? UserProfileIcon : ProfileIcon}
-              alt="로그인된 사용자 이미지"
-              className="w-full h-full object-cover cursor-pointer"
-            />
-          )}
         </div>
 
-        <div className="absolute top-[124px] left-1/2 -translate-x-1/2 w-[1171px]">
+        <div className="absolute top-[124px] left-1/2 -translate-x-1/2 w-[1171px] flex flex-col gap-[85px]">
           <div className="w-max bg-white rounded-[1000px] py-[10px] relative">
-            <FilterBar onModelChange={() => {}} onSortChange={() => {}} onlyFree={undefined} setOnlyFree={() => {}} />
+            <>
+              <section className="h-11 px-5 inline-flex justify-center items-center gap-3.5 font-['Spoqa_Han_Sans_Neo'] text-text-on-background text-xl font-medium">
+                {['모델', '필터', '태그'].map((label) => (
+                  <div key={label} className="relative p-2.5 flex justify-start items-center gap-2.5 cursor-pointer">
+                    <span>{label}</span>
+
+                    <div className={`w-6 h-6 p-1.5 rounded-full flex justify-center items-center`}>
+                      <img src={arrowDown} alt="arrow" className="w-3 h-3" />
+                    </div>
+                  </div>
+                ))}
+
+                <label className="ml-2 flex items-center gap-1 px-2 py-1 text-lg cursor-pointer">
+                  <span>무료만 보기</span>
+                  <input disabled type="checkbox" className="accent-primary-pressed w-4 h-4" />
+                </label>
+              </section>
+            </>
             <div className="absolute left-[274px] top-[76px] flex gap-[16px] w-full">
               <div>
                 <img src={ArrowFilter} alt="arrow-filter" />
               </div>
               <div className="text-white text-[18px] font-normal leading-[23px] mt-[10px]">
+                원하는 프롬프트를 바로 찾는
+                <br />
+                <span className="text-secondary-pressed">정렬 기능</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex relative left-[130px] gap-[43px]">
+            <div className="flex items-center justify-center">
+              <div className="relative w-[79px] height-[546px]">
+                <img src={Line} alt="line" className="w-full h-full object-contain" />
+              </div>
+              <div className="absolute animate-[slideDown_1.5s_linear_infinite]  ">
+                <style>
+                  {`
+                  @keyframes slideDown{
+                    0% {
+                      top:5%;
+                    }
+                    100% {
+                      top:80%;
+                    }
+                  }
+                `}
+                </style>
+                <img src={Cursor} alt="cursor" />
+              </div>
+            </div>
+            <div className="flex flex-col justify-end gap-[38px]">
+              <div className="text-white text-[32px] font-bold leading-[40px]">
                 <p>
-                  원하는 프롬프트를 바로 찾는
-                  <br />
-                  <span className="text-secondary-pressed">정렬 기능</span>
+                  원하는 <span className="text-secondary-pressed">프롬프트</span>를 <br />
+                  살펴보고 다운받으세요!
                 </p>
+              </div>
+              <div className="w-[170px] h-[184.87px]">
+                <img src={Mouse} alt="mouse" className="w-full h-full object-contain" />
               </div>
             </div>
           </div>
@@ -150,45 +188,9 @@ const CoachMark = ({ setShowCoachMark }: CoachMarkProps) => {
               <img src={ArrowFollow} alt="arrow-follow" />
             </div>
             <div className="text-white text-[18px] font-normal leading-[23px] mt-[20px] -translate-x-1/4">
-              <p>
-                관심있는 프롬프터
-                <br />
-                <span className="text-secondary-pressed">팔로우</span>하고 알림 받기
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex absolute top-[274px] left-[227px] gap-[52px]">
-          <div className="flex items-center justify-center">
-            <div className="relative w-[79px] height-[546px]">
-              <img src={Line} alt="line" className="w-full h-full object-contain" />
-            </div>
-            <div className="absolute animate-[slideDown_1.5s_linear_infinite]  ">
-              <style>
-                {`
-                  @keyframes slideDown{
-                    0% {
-                      top:5%;
-                    }
-                    100% {
-                      top:80%;
-                    }
-                  }
-                `}
-              </style>
-              <img src={Cursor} alt="cursor" />
-            </div>
-          </div>
-          <div className="flex flex-col justify-end gap-[38px]">
-            <div className="text-white text-[32px] font-bold leading-[40px]">
-              <p>
-                원하는 <span className="text-secondary-pressed">프롬프트</span>를 <br />
-                살펴보고 다운받으세요!
-              </p>
-            </div>
-            <div className="w-[170px] h-[184.87px]">
-              <img src={Mouse} alt="mouse" className="w-full h-full object-contain" />
+              관심있는 프롬프터
+              <br />
+              <span className="text-secondary-pressed">팔로우</span>하고 알림 받기
             </div>
           </div>
         </div>
@@ -196,8 +198,8 @@ const CoachMark = ({ setShowCoachMark }: CoachMarkProps) => {
 
       <div className="fixed bottom-4 justify-center items-center flex flex-col gap-2.5 left-1/2">
         <div className="flex gap-[17.88px] absolute left-[48px] bottom-[91px] whitespace-nowrap w-full">
-          <div>
-            <img src={ArrowCreate} alt="arrow-create" />
+          <div className="w-[55.73px] h-[43.54px] shrink-0">
+            <img src={ArrowCreate} alt="arrow-create" className="w-full h-full object-contain" />
           </div>
 
           <div className="text-white text-[18px] font-normal leading-[23px] relative bottom-[40px]">
