3e6a3843844ff0acc9cca27a9226ec5c39c832f0
jinhyo <kjhyo0220@gmail.com>
2025-07-29 04:42:49 +0900

    💡 Add: 화질 이슈로 인한 사진 추가 (#66)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { useAuth } from '@/context/AuthContext';
 import LogoIcon from '@assets/icon-header-logo.svg';
 import ProfileIcon from '@assets/icon-profile-blue-small.svg';
 import UserProfileIcon from '@assets/img-example-profile2.jpg';
-import TipIcon from '@assets/mobile/icon-mobile-tip.svg';
+import TipIcon from '@assets/mobile/icon-mobile-tip.png';
 
 import PrimaryButton from '@components/Button/PrimaryButton';
 import { HiMagnifyingGlass } from 'react-icons/hi2';
