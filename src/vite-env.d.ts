7de1badfc5211315e2df78dfe282c55047e1801a
jinhyo <kjhyo0220@gmail.com>
2025-08-01 03:37:39 +0900

    ✨ Feat: axiosInstance 세팅 (#74)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 /// <reference types="vite-plugin-pwa/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SERVER_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
