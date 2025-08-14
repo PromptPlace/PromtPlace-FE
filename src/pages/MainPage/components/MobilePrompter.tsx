6962deea5ab4e17a84029826e60b1d1ee65e4c6f
hairyung2002 <hairyung2002@naver.com>
2025-08-14 14:18:47 +0900

    🐛 Fix:메인페이지 모바일 팔로우(#112)

--- src/pages/MainPage/components/MobilePrompter.tsx
+++ src/pages/MainPage/components/MobilePrompter.tsx
@@ -15,63 +15,59 @@ interface MobilePrompterProps {
 }
 
 const MobilePrompter = ({ user }: MobilePrompterProps) => {
-  if (!user)
-    return null; // user가 없으면 렌더링하지 않음
-  else {
-    const { accessToken } = useAuth();
-    const [loginModalShow, setLoginModalShow] = useState(false);
-    const navigate = useNavigate();
+  const { accessToken } = useAuth();
+  const [loginModalShow, setLoginModalShow] = useState(false);
+  const navigate = useNavigate();
 
-    // 전체 팔로워 조회 후 팔로워 목록 조회
-    // const { data: followerData } = useGetFollower({ });
-    // 전체 유저 조회 후 팔로우 여부 판단 로직에 의해 useState 초기값 설정...?
-    const [isFollowed, setIsFollowed] = useState(false);
+  // 전체 팔로워 조회 후 팔로워 목록 조회
+  // const { data: followerData } = useGetFollower({ });
+  // 전체 유저 조회 후 팔로우 여부 판단 로직에 의해 useState 초기값 설정...?
+  const [isFollowed, setIsFollowed] = useState(false);
 
-    const { mutate: mutateFollow } = usePatchFollow({ member_id: user.user_id });
-    const { mutate: mutateUnFollow } = useDeleteFollow({ member_id: user.user_id });
+  const { mutate: mutateFollow } = usePatchFollow({ member_id: user.user_id });
+  const { mutate: mutateUnFollow } = useDeleteFollow({ member_id: user.user_id });
 
-    const handleFollow = () => {
-      if (isFollowed) {
-        mutateUnFollow({ member_id: user.user_id });
-      } else {
-        mutateFollow({ member_id: user.user_id });
-      }
-      setIsFollowed((prev) => !prev);
-    };
+  const handleFollow = () => {
+    if (isFollowed) {
+      mutateUnFollow({ member_id: user.user_id });
+    } else {
+      mutateFollow({ member_id: user.user_id });
+    }
+    setIsFollowed((prev) => !prev);
+  };
 
-    return (
-      <div className="w-full h-12 py-1.5 flex justify-between items-center bg-[#F5F5F5]">
-        {loginModalShow && (
-          <SocialLoginModal isOpen={loginModalShow} onClose={() => setLoginModalShow(false)} onClick={() => {}} />
-        )}
-        <div className="flex items-center gap-2 cursor-pointer" onClick={() => navigate(`/profile/${user.user_id}`)}>
-          <div className="w-[36px] h-[36px] rounded-full bg-gray-200 flex justify-center items-center">
-            <img
-              src={user.profile_img_url ? user.profile_img_url : userImage}
-              alt="authorImage"
-              className="w-full h-full rounded-full"
-            />
-          </div>
-          <div className="max-w-44 text-black text-xs font-medium uppercase leading-relaxed tracking-wide truncate">
-            {user.nickname}
-          </div>
+  return (
+    <div className="w-full h-12 py-1.5 flex justify-between items-center bg-[#F5F5F5]">
+      {loginModalShow && (
+        <SocialLoginModal isOpen={loginModalShow} onClose={() => setLoginModalShow(false)} onClick={() => {}} />
+      )}
+      <div className="flex items-center gap-2 cursor-pointer" onClick={() => navigate(`/profile/${user.user_id}`)}>
+        <div className="w-[36px] h-[36px] rounded-full bg-gray-200 flex justify-center items-center">
+          <img
+            src={user.profile_img_url ? user.profile_img_url : userImage}
+            alt="authorImage"
+            className="w-full h-full rounded-full"
+          />
         </div>
-        <FollowButton
-          follow={isFollowed}
-          onClick={() => {
-            if (!accessToken) {
-              alert('로그인이 필요합니다.');
-              setLoginModalShow(true);
-            } else {
-              handleFollow();
-            }
-          }}
-          size="sm"
-          type="button"
-        />
+        <div className="max-w-44 text-black text-xs font-medium uppercase leading-relaxed tracking-wide truncate">
+          {user.nickname}
+        </div>
       </div>
-    );
-  }
+      <FollowButton
+        follow={isFollowed}
+        onClick={() => {
+          if (!accessToken) {
+            alert('로그인이 필요합니다.');
+            setLoginModalShow(true);
+          } else {
+            handleFollow();
+          }
+        }}
+        size="sm"
+        type="button"
+      />
+    </div>
+  );
 };
 
 export default MobilePrompter;
