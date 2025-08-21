5d3135a7fdc51e9f4e24d32cfcd3c192e90b8ff0
fbehdgus <danny90697@gmail.com>
2025-08-21 18:12:46 +0900

    💄 Design: 계좌 정복 등록 페이지 뒤로가기 버튼 왼쪽정렬(#185)

--- src/pages/MyPage/MyAccountPage.tsx
+++ src/pages/MyPage/MyAccountPage.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import iconPerson from '@assets/icon-person-blue.svg';
 import bluearrowIcon from '@assets/icon-arrow-left-blue.svg'; //추후 디자인 규격에 맞게 수정 필요
-import blackarrowIcon from '@/assets/icon-arrow-left-black.svg';
+import blackarrowIcon from '@/assets/icon-arrow-black-thin.svg';
 import AccountDisplay from './components/AccountDisplay';
 import AccountEditForm from './components/AccountEditForm';
 import { useGetAccountInfo } from '@/hooks/queries/MyPage/useGetAccount';
@@ -77,12 +77,12 @@ const MyAccountPage = () => {
         </div>
 
         <div className="relative flex items-center  h-[90px] max-lg:h-auto border-b-[1px] max-lg:border-b-[0px] border-primary-hover">
-          <div className="flex justify-center max-lg:justify-center gap-[10px] h-[50px] max-lg:h-auto max-lg:w-full">
+          <div className="flex justify-center  gap-[10px] h-[50px] max-lg:h-auto max-lg:w-full">
             <button
               onClick={() => navigate(-1)}
-              className=" flex  justify-center   w-[24px] max-lg:w-[20px] h-[24px] max-lg:h-[20px] max-lg:mr-[66px]">
+              className="absolute left-0 flex  justify-center   w-[24px] max-lg:w-[20px] h-[24px] max-lg:h-[20px] max-lg:mr-[66px]">
               <img src={bluearrowIcon} alt="뒤로가기" className="max-lg:hidden" />
-              <img src={blackarrowIcon} alt="뒤로가기" className="lg:hidden" />
+              <img src={blackarrowIcon} alt="뒤로가기" className="lg:hidden w-[10px]" />
             </button>
             <span className="text-[24px] max-lg:text-[16px] text-primary-hover max-lg:text-[#2A2A2A] font-bold max-lg:font-medium">
               계좌 정보 등록
