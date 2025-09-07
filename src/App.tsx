29c68a70825b2a8bccd5c8229c2830f6148b497e
jinhyo <kjhyo0220@gmail.com>
2025-09-07 17:10:48 +0900

    🐛 Fix: 배포 버전에서만 GTM 연결 되도록 (#205)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@routes/pageRoutes';
 import { AuthProvider } from './context/AuthContext';
+import GTMScript from './utils/gtmScript';
 
 export const queryClient = new QueryClient();
 
@@ -11,9 +12,12 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
+        <GTMScript />
+
         <div className="max-lg:max-w-[425px] max-lg:w-full max-lg:mx-auto">
           <RouterProvider router={router} />
         </div>
+
         {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
       </AuthProvider>
     </QueryClientProvider>
