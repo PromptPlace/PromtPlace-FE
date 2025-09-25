331a2bb6196f79d4a42ab86fb18137373e2ee7c3
fbehdgus <danny90697@gmail.com>
2025-09-25 12:30:58 +0900

    ✨ Feat: 관리자 페이지 라우팅 처리(#230)

--- src/routes/pageRoutes.tsx
+++ src/routes/pageRoutes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, type RouteObject } from 'react-router-dom';
 import { lazyRoutes } from './routes';
 import HomeLayout from '@/layouts/HomeLayout';
 import ProtectedLayout from '@/layouts/ProtectedLayout';
+import AdminLayout from '@/layouts/AdminLayout';
 import TestPage from '@/pages/TestPage';
 
 export const publicRoutes: RouteObject[] = [
@@ -48,4 +49,14 @@ export const protectedRoutes: RouteObject[] = [
   },
 ];
 
-export const router = createBrowserRouter([...publicRoutes, ...protectedRoutes]);
+export const adminRoutes: RouteObject[] = [
+  {
+    path: '/admin',
+    element: <AdminLayout />,
+    children: [
+      //ex: { path: 'report', element: <lazyRoutes.AdminReportPage /> },
+    ],
+  },
+];
+
+export const router = createBrowserRouter([...publicRoutes, ...protectedRoutes, ...adminRoutes]);
