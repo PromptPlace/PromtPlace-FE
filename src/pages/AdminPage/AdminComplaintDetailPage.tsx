d1d594775cd4346c480979d2863719459182d649
fbehdgus <danny90697@gmail.com>
2025-09-30 16:57:49 +0900

    ♻️ Refactor: 본문 아래 border 가로 길이 수정(#234)

--- src/pages/AdminPage/AdminComplaintDetailPage.tsx
+++ src/pages/AdminPage/AdminComplaintDetailPage.tsx
@@ -18,7 +18,7 @@ interface ComplaintDetailPageProps {
   complaintDetail: ComplaintDetail;
 }
 
-const AdminComplaintDetailPage = ({complaintDetail}: ComplaintDetailPageProps) => {
+const AdminComplaintDetailPage = ({ complaintDetail }: ComplaintDetailPageProps) => {
   const navigate = useNavigate();
   return (
     <div className="w-[994px]  mx-auto mt-[39px] bg-white ">
@@ -34,17 +34,21 @@ const AdminComplaintDetailPage = ({complaintDetail}: ComplaintDetailPageProps) =
           <h1 className="text-[32px] font-bold px-[65px]">프롬프트 제목</h1>
           <div className="flex flex-col text-[20px] font-medium text-text-on-background gap-[19px] px-[65px]">
             <div className="flex gap-[94px] py-[9px]">
-            <p>date</p>
-            <p>신고자:</p>
+              <p>date</p>
+              <p>신고자:</p>
             </div>
             <p>사유:</p>
           </div>
         </section>
-        <section className="py-[30px] border-b-[1px] border-white-stroke  h-[425px] overflow-y-auto px-[65px]">description</section>
+        <section className="py-[30px] border-b-[1px] border-white-stroke  h-[425px] overflow-y-auto   mx-[65px]">
+          description
+        </section>
       </main>
 
       <footer className="py-[40px] pl-[65px]">
-        <button className="px-[20px] py-[10px] border-[1px] border-alert rounded-[10px] text-[20px] font-medium text-alert ">이동</button>
+        <button className="px-[20px] py-[10px] border-[1px] border-alert rounded-[10px] text-[20px] font-medium text-alert ">
+          이동
+        </button>
       </footer>
     </div>
   );
