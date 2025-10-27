0523c1a2311e1e142a250b586806331308611de7
jinhyo <kjhyo0220@gmail.com>
2025-10-27 16:04:33 +0900

    💄 Design: 헤더 모바일 반응형 추가 (#243)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -28,10 +28,10 @@ const Navbar = () => {
   const { data } = useGetMember({ member_id: user.user_id });
 
   const LINKS = [
-    { to: '/', label: '홈' },
-    { to: '/prompt/10', label: '프롬프트 보기' },
-    { to: '/guide/tip', label: 'AI 꿀팁' },
-    { to: '/guide/notice', label: '공지사항' },
+    { to: '/', label: '홈', labelMobile: '홈' },
+    { to: '/prompt/10', label: '프롬프트 보기', labelMobile: '프롬프트' },
+    { to: '/guide/tip', label: 'AI 꿀팁', labelMobile: 'AI 꿀팁' },
+    { to: '/guide/notice', label: '공지사항', labelMobile: '공지' },
   ];
 
   useEffect(() => {
@@ -83,7 +83,7 @@ const Navbar = () => {
           )}
 
           {accessToken && (
-            <div className="flex gap-[26px] items-center cursor-pointer">
+            <div className="flex gap-[26px] max-phone:gap-[18px] items-center cursor-pointer">
               <BackgroundButton
                 background="secondary"
                 text="프롬프트 올리기"
@@ -94,33 +94,34 @@ const Navbar = () => {
               <img
                 src={NotificationIcon}
                 alt="알림"
-                className="self-center"
+                className="self-center max-phone:w-[16px] max-phone:h-[16px]"
                 onClick={() => navigate('/mypage/message/notification')}
               />
               <img
                 src={MessageIcon}
                 alt="메세지 알림"
-                className="self-center"
+                className="self-center max-phone:w-[16px] max-phone:h-[16px]"
                 onClick={() => navigate('/mypage/message/message')}
               />
               <img
                 src={data?.data.profile_image || UserIcon}
                 alt="프로필 이미지"
-                className="w-[40px] object-cover self-center"
+                className="w-[40px] object-cover self-center max-phone:hidden"
                 onClick={() => navigate(`/profile/${user.user_id}`)}
               />
             </div>
           )}
         </div>
 
-        <div className="px-[102px] max-lg:px-[20px] py-[16px] flex justify-between items-center custom-h3 text-black whitespace-nowrap">
+        <div className="px-[102px] max-lg:px-[20px] py-[16px] flex justify-between items-center custom-h3 max-phone:text-[14px] text-black whitespace-nowrap">
           <div className="flex items-center gap-[20px] max-lg:gap-[8px]">
             {LINKS.map((link) => (
               <NavLink
                 to={link.to}
                 key={link.to}
-                className={`${link.label === '/' ? 'pr-[8px]' : 'px-[8px]'} hover:font-medium hover:text-primary`}>
-                {link.label}
+                className={`${link.to === '/' ? 'pr-[8px]' : 'px-[8px]'} hover:font-medium hover:text-primary`}>
+                <p className="max-phone:hidden">{link.label}</p>
+                <p className="phone:hidden">{link.labelMobile}</p>
               </NavLink>
             ))}
           </div>
