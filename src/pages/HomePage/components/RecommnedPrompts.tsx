91fe32f1703dc90c0afe5d5b1a974c690af1ad3a
jinhyo <kjhyo0220@gmail.com>
2025-11-01 04:07:32 +0900

    ✨ Feat: 추천 프롬프트 일부 구현 (#243)

--- src/pages/HomePage/components/RecommnedPrompts.tsx
+++ src/pages/HomePage/components/RecommnedPrompts.tsx
@@ -1,5 +1,9 @@
 const RecommnedPrompts = () => {
-  return <div>관리하자 추천하는 프롬프트 TOP8</div>;
+  return (
+    <div className="mt-[104px] px-[102px] max-lg:px-[20px] max-phone:px-[40px]">
+      <p className="custom-h2">관리하자 추천하는 프롬프트 TOP8</p>
+    </div>
+  );
 };
 
 export default RecommnedPrompts;
