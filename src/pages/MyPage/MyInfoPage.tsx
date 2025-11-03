3eb9617e2c7b627b92652dc0ae8c33c57273641d
fbehdgus <danny90697@gmail.com>
2025-11-03 21:56:15 +0900

    ♻️ Refactor: (구)myinfopage 수정(#250)

--- src/pages/MyPage/MyInfoPage.tsx
+++ src/pages/MyPage/MyInfoPage.tsx
@@ -7,6 +7,10 @@ import { useNavigate } from 'react-router-dom';
 import { withdrawUser } from '@/apis/MyPage/withdrawl';
 import { useAuth } from '@/context/AuthContext';
 import useGetMember from '@/hooks/queries/ProfilePage/useGetMember';
+import GoogleIcon from '@/assets/icon-google-logo.svg';
+import KakaoIcon from '@/assets/icon-kakao-logo.svg';
+import NaverIcon from '@/assets/icon-naver-logo.svg';
+import PrimaryButton from '@/components/Button/PrimaryButton';
 
 // 실제로는 API로 받아올 사용자 정보
 const userInfo: {
@@ -24,7 +28,7 @@ const MyPageInfo = () => {
   const confirmDelete = () => setDeleteStep('confirm');
   const showWarning = () => setDeleteStep('warning');
 
-  const { logout, user } = useAuth();
+  const { login, logout, user } = useAuth();
   const { data: userData } = useGetMember({ member_id: user.user_id });
 
   const deleteAccount = () => {
@@ -47,74 +51,105 @@ const MyPageInfo = () => {
     setDeleteStep(null);
   };
 
+  const getProviderIcon = (provider: 'GOOGLE' | 'KAKAO' | 'NAVER') => {
+    switch (provider) {
+      case 'GOOGLE':
+        return <img src={GoogleIcon} alt="Google Icon" className="inline-block w-[24px] h-[24px]" />;
+      case 'KAKAO':
+        return <img src={KakaoIcon} alt="Kakao Icon" className="inline-block w-[24px] h-[24px]" />;
+      case 'NAVER':
+        return <img src={NaverIcon} alt="Naver Icon" className="inline-block w-[24px] h-[24px]" />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="flex justify-center pt-[92px] max-lg:pt-[12px] min-h-screen bg-background ">
-      <div className="w-full max-w-[1236px] max-lg:px-[20px] ">
-        <div className="flex items-center gap-[10px] mb-[70px] max-lg:mb-[20px]">
-          <img src={iconPerson} alt="person icon" className="w-[32px] max-lg:w-[20px] h-[32px] max-lg:h-[20px]" />
-          <div className="text-[32px] max-lg:text-[20px] text-primary-hover font-bold">회원정보</div>
-        </div>
+      <div className="w-full max-lg:px-[20px]">
+        <label className="custom-h2 text-text-on-white">프로필</label>
+        <section className="flex-flex-col gap-[20px] bg-white  mt-[20px] p-[24px] rounded-[12px]">
+          <div>
+            <label className="custom-h5 block mb-[12px]">닉네임</label>
+            <p className="custom-body2 text-text-on-white px-[16px] py-[12px]">{userData?.data.nickname}</p>
+          </div>
+          <div>
+            <label className="custom-h5 block mb-[12px]">프로필 사진</label>
+            <p className="h-[120px] w-[120px]">프로필 사진</p>
+          </div>
+          <div className="flex flex-col gap-[12px]">
+            <label className="custom-h5 block">SNS</label>
+            <label className="custom-button2 block text-gray-700">SNS 아이디</label>
+            <p className="text-gray-400 custom-body2 px-[16px] py-[12px]"> 아직 작성하지 않았어요!</p>
+            <label className="custom-button2 block text-gray-700">접속 가능한 URL</label>
+            <p className="text-gray-400 custom-body2 px-[16px] py-[12px]"> 아직 작성하지 않았어요!</p>
+          </div>
+          <div className="flex flex-col gap-[12px]">
+            <label className="custom-h5 block">소개말</label>
+            <p className="text-gray-400 custom-body2 px-[16px] py-[12px]"> 아직 작성하지 않았어요!</p>
+          </div>
+        </section>
+        <label className="custom-h2 text-text-on-white">계정</label>
+        <section className="flex-flex-col gap-[20px] bg-white  mt-[20px] p-[24px] rounded-[12px]">
+          <div>
+            <label className="custom-h5 block mb-[12px]">가입한 계정</label>
+            <div className="flex gap-[12px] custom-body2 text-text-on-white px-[16px] py-[12px]">
+              {getProviderIcon(user.social_type)}
+              {userData?.data.email}
+            </div>
+          </div>
+          <div>
+            <label className="custom-h5 block mb-[12px]">계정 바꾸기</label>
+            <div className="flex justify-between custom-body2 text-text-on-white px-[16px] py-[12px]">
+              <div className="flex gap-[12px]">
+                {getProviderIcon(user.social_type)}
+                {userData?.data.email}
+              </div>
+              <PrimaryButton buttonType="square" text="변경하기" onClick={() => {}} />
+            </div>
+          </div>
+          <div>
+            <label className="custom-h5 block mb-[12px]">로그아웃 및 회원탈퇴</label>
+            <div className="flex justify-end gap-[12px] custom-body2 text-text-on-white px-[16px] py-[12px]">
+              <PrimaryButton buttonType="square" text="로그아웃" onClick={logout} />
+              <PrimaryButton buttonType="square" text="탈퇴하기" onClick={confirmDelete} />
+            </div>
+          </div>
+        </section>
 
-        <div className="bg-white border-t-[1px] max-lg:border-t-[0px] border-t-primary-hover">
-          <InfoRow label="닉네임" nickname={userData?.data.nickname} />
-          <InfoRow label="가입한 계정" email={userData?.data.email} provider={user.social_type} />
-          <InfoRow label="계정 바꾸기" email={userData?.data.email} provider={user.social_type} hasArrow />
-          <InfoRow
-            label="계좌 정보 등록"
-            actionText="등록하기"
-            onAction={() => {
-              navigate('/mypage/info/account');
+        {deleteStep === 'confirm' && (
+          <DualModal
+            text="회원을 탈퇴하시겠습니까?"
+            onClickYes={showWarning}
+            onClickNo={closeModal}
+            colorYesText="white"
+            colorNoText="blue"
+          />
+        )}
+        {deleteStep === 'warning' && (
+          <DualModal
+            text={
+              <>
+                탈퇴하시면 회원님의 모든 데이터가 삭제되어 다시 복구할 수 없습니다.
+                <br />
+                정말 탈퇴하시겠습니까?
+              </>
+            }
+            onClickYes={deleteAccount}
+            onClickNo={closeModal}
+            colorYesText="white"
+            colorNoText="blue"
+          />
+        )}
+        {deleteStep === 'complete' && (
+          <SingleModal
+            text="탈퇴가 완료되었습니다."
+            onClick={() => {
+              closeModal();
+              navigate('/');
             }}
           />
-          <InfoRow
-            label="결제내역"
-            actionText="내역보기"
-            onAction={() => {
-              navigate('/mypage/paymentHistory');
-            }}
-          />
-          <InfoRow
-            label="로그아웃"
-            actionText="로그아웃"
-            onAction={() => {
-              logout();
-            }}
-          />
-          <InfoRow label="계정 탈퇴" actionText="탈퇴하기" onAction={confirmDelete} isDestructive />
-          {deleteStep === 'confirm' && (
-            <DualModal
-              text="회원을 탈퇴하시겠습니까?"
-              onClickYes={showWarning}
-              onClickNo={closeModal}
-              colorYesText="white"
-              colorNoText="blue"
-            />
-          )}
-          {deleteStep === 'warning' && (
-            <DualModal
-              text={
-                <>
-                  탈퇴하시면 회원님의 모든 데이터가 삭제되어 다시 복구할 수 없습니다.
-                  <br />
-                  정말 탈퇴하시겠습니까?
-                </>
-              }
-              onClickYes={deleteAccount}
-              onClickNo={closeModal}
-              colorYesText="white"
-              colorNoText="blue"
-            />
-          )}
-          {deleteStep === 'complete' && (
-            <SingleModal
-              text="탈퇴가 완료되었습니다."
-              onClick={() => {
-                closeModal();
-                navigate('/');
-              }}
-            />
-          )}
-        </div>
+        )}
       </div>
     </div>
   );
