8c9fb7bf6ebe4c4d191d6efa17491102b1585e20
fbehdgus <danny90697@gmail.com>
2025-11-04 05:32:46 +0900

    ✨ Feat: 비밀번호 변경 관련 인증코드 발급 api 연결(#250)

--- src/components/Modal/components/forgotPassword.tsx
+++ src/components/Modal/components/forgotPassword.tsx
@@ -1,87 +1,121 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import PrimaryButton from '@components/Button/PrimaryButton';
 import type { ModalView } from '@/types/LoginPage/auth';
+import useRequestResetPasswordAuthCode from '@/hooks/mutations/LoginPage/useRequestResetPasswordAuthCode';
+import useVerifyResetPasswordAuthcode from '@/hooks/mutations/LoginPage/useVerifyResetPasswordAuthCode';
 
 interface LoginViewProps {
   setView: (view: ModalView) => void;
+  authCode: string;
+  setAuthCode: (authCode: string) => void;
 }
 type EmailStatus = 'default' | 'sending' | 'sent' | 'resend' | 'verified';
 type CodeStatus = 'idle' | 'request' | 'verified' | 'error';
-
-const handleSendCode = () => {
-  // 인증번호 발송 로직 구현
-};
-
-const renderEmailAccessory = ({ emailStatus }: { emailStatus: EmailStatus }) => {
-  switch (emailStatus) {
-    case 'default': // '인증번호 발송' 버튼
-      return <PrimaryButton buttonType="square" text="인증번호 발송" py={6} px={12} textSize={12} onClick={() => {}} />;
-    case 'sending': // '전송 중' (로딩 스피너 등)
-      return <PrimaryButton buttonType="square" text="전송 중" py={6} px={12} textSize={12} onClick={() => {}} />;
-
-    case 'sent': // '02:54' 타이머
-      // (여기서 Timer 컴포넌트를 렌더링하고,
-      //  시간이 0이 되면 setEmailStatus('error')로 변경)
-      return <span>02:54</span>; // <Timer onEnd={() => setEmailStatus('error')} />
-
-    case 'resend': // '재발송' 버튼
-      return <PrimaryButton buttonType="square" text="재발송" py={6} px={12} textSize={12} onClick={() => {}} />;
-    case 'verified': // '인증 완료' 텍스트
-      return (
-        <PrimaryButton
-          buttonType="square"
-          text="인증 완료"
-          textColor="white"
-          py={6}
-          px={12}
-          textSize={12}
-          onClick={() => {}}
-        />
-      );
-
-    default:
-      return null;
-  }
-};
-
-const renderCodeAccessory = ({ codeStatus }: { codeStatus: CodeStatus }) => {
-  switch (codeStatus) {
-    case 'verified': // '인증 완료' 텍스트
-      return (
-        <PrimaryButton
-          buttonType="square"
-          text="인증 완료"
-          textColor="white"
-          py={6}
-          px={12}
-          textSize={12}
-          onClick={() => {}}
-        />
-      );
-
-    case 'request': // '인증 확인'
-      return <PrimaryButton buttonType="square" text="인증 확인" py={6} px={12} textSize={12} onClick={() => {}} />;
-  }
-};
-
-const handleVerifyCode = () => {
-  // 인증번호 확인 로직
-};
 
 const handleSubmit = (e: React.FormEvent) => {
   e.preventDefault();
   // 여기에 회원가입 로직 추가
 };
 
-const ForgotPasswordView = ({ setView }: LoginViewProps) => {
+const ForgotPasswordView = ({ setView, authCode, setAuthCode }: LoginViewProps) => {
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
-  const [verificationCode, setVerificationCode] = useState('');
   const [verificationCodeError, setVerificationCodeError] = useState('');
   const [emailStatus, setEmailStatus] = useState<EmailStatus>('default');
   const [codeStatus, setCodeStatus] = useState<CodeStatus>('request');
   const isDisabled = emailStatus !== 'verified' || codeStatus !== 'verified';
+
+  const { mutate: requestResetPasswordAuthCode } = useRequestResetPasswordAuthCode();
+  const { mutate: verifyResetPasswordAuthCode } = useVerifyResetPasswordAuthcode();
+  const handleSendCode = () => {
+    // 인증번호 발송 로직
+    requestResetPasswordAuthCode(email, {
+      onSuccess: () => {
+        setEmailStatus('sent');
+      },
+      onError: (error) => {
+        setEmailError('이메일 인증 요청에 실패했습니다. 다시 시도해주세요.');
+        console.error('이메일 인증 요청 실패:', error);
+      },
+    });
+  };
+  const handleVerifyCode = () => {
+    // 인증번호 확인 로직
+    verifyResetPasswordAuthCode(
+      { email, code: authCode },
+      {
+        onSuccess: () => {
+          setCodeStatus('verified');
+        },
+        onError: () => {
+          setVerificationCodeError('인증번호 확인에 실패했습니다. 다시 시도해주세요.');
+          console.error('인증번호 확인 실패:');
+        },
+      },
+    );
+  };
+  const renderEmailAccessory = ({ emailStatus }: { emailStatus: EmailStatus }) => {
+    switch (emailStatus) {
+      case 'default': // '인증번호 발송' 버튼
+        return (
+          <PrimaryButton
+            buttonType="square"
+            text="인증번호 발송"
+            py={6}
+            px={12}
+            textSize={12}
+            onClick={handleSendCode}
+          />
+        );
+      case 'sending': // '전송 중' (로딩 스피너 등)
+        return <PrimaryButton buttonType="square" text="전송 중" py={6} px={12} textSize={12} onClick={() => {}} />;
+
+      case 'sent': // '02:54' 타이머
+        // (여기서 Timer 컴포넌트를 렌더링하고,
+        //  시간이 0이 되면 setEmailStatus('error')로 변경)
+        return <span>02:54</span>; // <Timer onEnd={() => setEmailStatus('error')} />
+
+      case 'resend': // '재발송' 버튼
+        return <PrimaryButton buttonType="square" text="재발송" py={6} px={12} textSize={12} onClick={() => {}} />;
+      case 'verified': // '인증 완료' 텍스트
+        return (
+          <PrimaryButton
+            buttonType="square"
+            text="인증 완료"
+            textColor="white"
+            py={6}
+            px={12}
+            textSize={12}
+            onClick={() => {}}
+          />
+        );
+
+      default:
+        return null;
+    }
+  };
+
+  const renderCodeAccessory = ({ codeStatus }: { codeStatus: CodeStatus }) => {
+    switch (codeStatus) {
+      case 'verified': // '인증 완료' 텍스트
+        return (
+          <PrimaryButton
+            buttonType="square"
+            text="인증 완료"
+            textColor="white"
+            py={6}
+            px={12}
+            textSize={12}
+            onClick={() => {}}
+          />
+        );
+
+      case 'request': // '인증 확인'
+        return (
+          <PrimaryButton buttonType="square" text="인증 확인" py={6} px={12} textSize={12} onClick={handleVerifyCode} />
+        );
+    }
+  };
 
   return (
     <div className="flex flex-col items-center w-full">
@@ -112,8 +146,8 @@ const ForgotPasswordView = ({ setView }: LoginViewProps) => {
               id="verificationCode"
               placeholder="숫자 네 자리를 입력해 주세요"
               className="w-full bg-background px-[16px] py-[12px] custom-body2 placeholder:text-gray-400 text-text-on-white mb-[12px] rounded-[8px]"
-              value={verificationCode}
-              onChange={(e) => setVerificationCode(e.target.value)}
+              value={authCode}
+              onChange={(e) => setAuthCode(e.target.value)}
             />
             <div className="absolute right-[16px] top-[8px]">{renderCodeAccessory({ codeStatus })}</div>
           </div>
