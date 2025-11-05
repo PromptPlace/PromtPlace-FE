4814a96c0ccb21e0ec78ab6813d5e033d886c9c4
jinhyo <kjhyo0220@gmail.com>
2025-11-05 19:34:26 +0900

    ✨ Feat: 추천 프롬프트 일부 연결 (#253)

--- src/pages/HomePage/components/RecommnedPrompts.tsx
+++ src/pages/HomePage/components/RecommnedPrompts.tsx
@@ -5,6 +5,9 @@ import PromptMobileCard from './PromptMobileCard';
 const RecommnedPrompts = () => {
   const { data } = useGetPromptList();
 
+  const top8Lists = [2119, 2118, 2120];
+  const displayData = data?.data.filter((prompt) => top8Lists.includes(prompt.prompt_id)).slice(0, 8);
+
   return (
     <div className="mt-[104px] mb-[72px] max-phone:mb-[56px]">
       <div className="px-[102px] max-lg:px-[40px] max-phone:px-[20px]">
@@ -13,7 +16,7 @@ const RecommnedPrompts = () => {
 
       <div className="max-phone:hidden mt-[40px] max-phone:mt-[20px] overflow-x-auto pl-[102px] pb-[24px] max-lg:pl-[40px] max-phone:px-[20px]">
         <div className="grid grid-cols-4 overflow-x-auto gap-[20px] min-w-[1236px] ">
-          {data?.data.slice(0, 8).map((prompt) => (
+          {displayData?.map((prompt) => (
             <PromptCard key={prompt.prompt_id} prompt={prompt} />
           ))}
         </div>
@@ -21,7 +24,7 @@ const RecommnedPrompts = () => {
 
       <div className="phone:hidden mt-[40px] max-phone:mt-[20px] pl-[102px] pb-[24px] max-lg:pl-[40px] max-phone:px-[20px]">
         <div className="flex flex-col gap-[8px]">
-          {data?.data.slice(0, 8).map((prompt) => (
+          {displayData?.map((prompt) => (
             <PromptMobileCard key={prompt.prompt_id} prompt={prompt} />
           ))}
         </div>
