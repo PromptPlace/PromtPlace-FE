9c1289cc6f9db0f3005f369272da26efb8c38e7a
fbehdgus <danny90697@gmail.com>
2025-11-06 11:44:05 +0900

    ✨ Feat:작성한 프롬프트 페이지 밑 컴포넌트 카드 구현(#250)

--- src/pages/MyPage/components/AuthoredPromptCard.tsx
+++ src/pages/MyPage/components/AuthoredPromptCard.tsx
@@ -2,40 +2,157 @@ import PrimaryButton from '@components/Button/PrimaryButton';
 import { Link } from 'react-router-dom';
 import type { NewAuthoredPromptDTO } from '@/types/MyPage/prompt';
 import logo from '@/assets/logo/app/app-logo-default.svg';
+import Portal from './Portal';
+import kebabMenu from '@/assets/icon-kebabMenu.svg';
+import { useState, useRef, useEffect } from 'react';
+import Rating from '@/components/Rating';
 
 interface AuthoredPromptCardProps {
   prompt: NewAuthoredPromptDTO;
+  DeletePrompt: (prompt_id: number) => void;
+  EditPrompt: (prompt_id: number) => void;
 }
 
 //여기는 하나의 프롬프트만 받아오면 됨
-const AuthoredPromptCard = ({ prompt }: AuthoredPromptCardProps) => {
+const AuthoredPromptCard = ({ prompt, DeletePrompt, EditPrompt }: AuthoredPromptCardProps) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const buttonRef = useRef<HTMLButtonElement>(null); // 버튼의 위치를 얻기 위한 ref
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const anchorRef = useRef<HTMLDivElement>(null);
+  const [menuPosition, setMenuPosition] = useState({ top: 0, left: 0 });
+
+  const lgQuery = '(min-width: 1024px)';
+
+  const getIsDesktop = () => window.matchMedia(lgQuery).matches;
+
+  useEffect(() => {
+    if (!isDropdownOpen) return;
+
+    const updatePosition = () => {
+      if (buttonRef.current) {
+        const rect = buttonRef.current.getBoundingClientRect();
+        const isDesktop = getIsDesktop();
+
+        const dropdownWidth = isDesktop ? 90 : 70;
+        const verticalOffset = isDesktop ? 2 : 0;
+
+        setMenuPosition({
+          top: rect.bottom + verticalOffset,
+          left: rect.right - dropdownWidth,
+        });
+      }
+    };
+
+    updatePosition();
+
+    window.addEventListener('scroll', updatePosition, true);
+    window.addEventListener('resize', updatePosition, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', updatePosition, true);
+      window.removeEventListener('resize', updatePosition);
+    };
+  }, [isDropdownOpen]); // isDropdownOpen이 바뀔 때만 이 로직을 실행
+
+  useEffect(() => {
+    if (!isDropdownOpen) return;
+
+    const handleClickOutside = (event: MouseEvent) => {
+      // 케밥 버튼(buttonRef)과 드롭다운 메뉴(dropdownRef) 바깥을 클릭했는지 확인
+      if (
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node) &&
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isDropdownOpen]);
+
   const imageUrl = prompt.image_url ? prompt.image_url : logo;
   return (
-    <div className="w-full bg-white pr-[24px]">
+    <div className="w-full bg-white pr-[24px] mb-[20px]">
       <div className="flex justify-between">
-        <div className="flex gap-[24px]">
+        <div className="flex gap-[24px] items-center">
           <img src={imageUrl} alt="프롬프트 이미지" className="w-[80px] h-[80px] rounded-[8px]" />
           <Link to={`/prompt/${prompt.prompt_id}`}>
             <p className="custom-h3 text-text-on-white">{prompt.title}</p>
           </Link>
         </div>
-        <div className="flex gap-[16px]">
-          <p>조회수</p>
-          <p>{prompt.views}</p>
-          <p>다운로드 수</p>
-          <p>{prompt.downloads}</p>
+        <div className="flex items-center gap-[16px]">
+          <div className="flex flex-col items-center">
+            <p className="custom-body3">조회수</p>
+            <p className="custom-button1">{prompt.views}</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="custom-body3">다운로드 수</p>
+            <p className="custom-button1">{prompt.downloads}</p>
+          </div>
+
+          <div className="flex items-center justify-center h-[72px] max-lg:h-auto shrink-0 w-[115px] max-lg:w-auto py-[10px] max-lg:py-[0px]  ">
+            <div className="relative" ref={anchorRef}>
+              <button
+                ref={buttonRef}
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                className={`flex items-center justify-center h-[28px] w-[28px] rounded-[50px]  ${isDropdownOpen ? 'bg-secondary-pressed' : 'bg-transparent'}`}>
+                <img src={kebabMenu} alt="케밥 메뉴" className="max-lg:w-[16px] max-lg:h-[16px]" />
+              </button>
+              {isDropdownOpen && (
+                <Portal>
+                  <div
+                    ref={dropdownRef}
+                    className="fixed mt-[11px] w-[91px] max-lg:w-[61px] bg-white rounded-md z-10 shadow-[0_4px_8px_0_rgba(0,0,0,0.12)]"
+                    style={{ top: `${menuPosition.top}px`, left: `${menuPosition.left}px` }}>
+                    <button
+                      onClick={() => {
+                        DeletePrompt(prompt.prompt_id);
+                        setIsDropdownOpen(false);
+                      }}
+                      className="block  px-[16px] max-lg:px-[12px] py-[8px] max-lg:py-[4px] text-[16px] max-lg:text-[10px] border-b-[1px] border-b-white-stroke text-text-on-background bg-secondary active:bg-secondary-pressed rounded-t-[4px]">
+                      삭제하기
+                    </button>
+                    <button
+                      onClick={() => {
+                        EditPrompt(prompt.prompt_id);
+                        setIsDropdownOpen(false);
+                      }}
+                      className="block px-[16px] max-lg:px-[12px] py-[8px] max-lg:py-[4px] text-[16px] max-lg:text-[10px] text-text-on-background bg-secondary active:bg-secondary-pressed rounded-b-[4px]">
+                      수정하기
+                    </button>
+                  </div>
+                </Portal>
+              )}
+            </div>
+          </div>
         </div>
       </div>
 
       {prompt.reviews.data.slice(0, 3).map((review) => (
         <div>
-          <div className="flex justify-between">
-            <div>
-              <span>{review.nickname}</span>
-              <span>{review.content}</span>
+          <div className="flex flex-col gap-[8px] px-[28px]">
+            <div className="flex justify-between">
+              <span className="custom-body3">{review.nickname}</span>
+              <span className="w-[80px] h-[16px]">
+                <Rating star={review.rating} />
+              </span>
             </div>
-            <span>{review.rating}</span>
+            <div className="custom-body3">{review.content}</div>
           </div>
+          //리뷰 개수 4개 이상일때 리뷰 더보기 버튼
+          {prompt.reviews.data.length > 4 && review === prompt.reviews.data[2] && (
+            <div className="flex justify-end pr-[28px] mb-[12px]">
+              <Link to={`/prompt/${prompt.prompt_id}`}>
+                <button className="custom-button2 text-gray-500">리뷰 더 보기 ({prompt.reviews.data.length})</button>
+              </Link>
+            </div>
+          )}
         </div>
       ))}
     </div>
--- src/pages/MyPage/components/AuthoredPromptPage.tsx
+++ src/pages/MyPage/components/AuthoredPromptPage.tsx
@@ -1,4 +1,15 @@
 import { useGetMyPrompts } from '@/hooks/queries/MyPage/useGetMyPrompt';
+import AuthoredPromptCard from './AuthoredPromptCard';
+import { useNavigate } from 'react-router-dom';
+import usePatchDeletePrompts from '@/hooks/mutations/ProfilePage/usePatchDeletePrompts';
+import type { RequestDeletePromptDto } from '@/types/ProfilePage/profile';
+import { useAuth } from '@/context/AuthContext';
+
+
+
+
+
+
 
 const AuthoredPromptPage = () => {
   const {
@@ -8,10 +19,37 @@ const AuthoredPromptPage = () => {
     isLoading,
     isError,
   } = useGetMyPrompts();
-  return <div>
-    <label>작성한 프롬프트</label>
 
-  </div>;
+  const navigate = useNavigate();
+  const EditAuthoredPrompt = (prompt_id: number) => {
+    const promptId = prompt_id;
+    const targetUrl = `/mypage/edit/${promptId}`;
+
+    // 3. 생성된 URL로 페이지를 이동시킵니다.
+    navigate(targetUrl);
+  };
+  const { user } = useAuth();
+  const { mutate: mutateDeletePrompts } = usePatchDeletePrompts({ member_id: user.user_id });
+  const handleDeleteAuthoredPrompts = ({ prompt_id }: RequestDeletePromptDto) => {
+    mutateDeletePrompts({ prompt_id });
+  };
+
+  return (
+    <div className="mt-[64px] w-full">
+      <label className="custom-h2 text-black">작성한 프롬프트</label>
+      <div className="ml-[20px] rounded-[50px] border-[0.8px] px-[10px] py-[5px] bg-white text-gray-500 custom-h5">{data?.prompts.length || 0}</div>
+      <div className="flex flex-col gap-[20px] mt-[20px] p-[24px] rounded-[12px]">
+        {data?.prompts.map((prompt) => (
+          <AuthoredPromptCard
+            key={prompt.prompt_id}
+            prompt={prompt}
+            DeletePrompt={() => handleDeleteAuthoredPrompts({ prompt_id: prompt.prompt_id })}
+            EditPrompt={() => EditAuthoredPrompt(prompt.prompt_id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AuthoredPromptPage;
