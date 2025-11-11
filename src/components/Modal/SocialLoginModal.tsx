36cf8a83d63004524583afa1e2ac83cb668f5415
fbehdgus <danny90697@gmail.com>
2025-11-12 03:55:44 +0900

    ♻️ Refactor: 일정 크기 이하로는 모달 크기 줄어들지 않게 수정(#307)

--- src/components/Modal/SocialLoginModal.tsx
+++ src/components/Modal/SocialLoginModal.tsx
@@ -42,7 +42,7 @@ const SocialLoginModal = ({ isOpen, onClose, initialView = 'login' }: SocialLogi
   const [scale, setScale] = useState(1);
   const BASE_WIDTH = 656; // 👈 모달의 기본 너비(w-[656px])
   const BASE_HEIGHT = 850;
-
+  const MIN_SCALE = 0.6;
   // 3. (✅ 추가) 윈도우 크기에 따라 scale 값을 계산하는 로직
   useLayoutEffect(() => {
     const handleResize = () => {
@@ -50,7 +50,8 @@ const SocialLoginModal = ({ isOpen, onClose, initialView = 'login' }: SocialLogi
       const heightScale = (window.innerHeight - 40) / BASE_HEIGHT;
       const base = Math.min(1, widthScale, heightScale); // 둘 중 더 작은 쪽 기준
       const newScale = Math.pow(base, 1.5); // ← 이 수치만 조절하면 됨
-      setScale(newScale);
+      const finalScale = Math.max(newScale, MIN_SCALE);
+      setScale(finalScale);
     };
 
     // 처음 마운트될 때와 윈도우 크기가 바뀔 때 실행
@@ -127,7 +128,7 @@ const SocialLoginModal = ({ isOpen, onClose, initialView = 'login' }: SocialLogi
     <div onClick={onClose} className="fixed inset-0 flex items-center justify-center bg-overlay bg-opacity-40 z-110">
       <div
         onClick={(e) => e.stopPropagation()}
-        className="relative flex  justify-center items-center max-w-[656px] max-h-[850px]  py-[48px]  flex-col px-[56px] py-[48] rounded-[16px]  bg-white shadow-gradient 
+        className="relative flex  justify-center items-center max-w-[656px] max-h-[850px]  py-[48px]  flex-col px-[56px] rounded-[16px]  bg-white shadow-gradient 
       origin-center"
         style={{ transform: `scale(${scale})` }}>
         <button className="flex absolute top-[48px] right-[56px] gap-[4px] py-[3px] h-[22px]" onClick={onClose}>
