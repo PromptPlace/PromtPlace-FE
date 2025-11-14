47f90abba6ae7d77f68b9d99c0e678728312982a
fbehdgus <danny90697@gmail.com>
2025-11-14 23:40:54 +0900

    🐛 Fix: localstorage 이용으로 인한 accessToken 공유 분제 해결(#330)

---
+++ src/hooks/useSessionStorage.ts
@@ -0,0 +1,41 @@
+/**
+ * 세션 스토리지와 관련된 로직을 처리하는 훅입니다.
+ * setItem 사용 시에는 저장할 값을 같이 넘겨주시면 됩니다.
+ *
+ * @param {string} key -- 세션스토리지 키 값
+ * @returns { setItem, getItem, removeItem }
+ *
+ * @example
+ * const { getItem, setItem, removeItem } = useSessionStorage(LOCAL_STORAGE_KEY.accessToken);
+ *
+ * @author 김진효
+ * **/
+
+export const useSessionStorage = (key: string) => {
+  const setItem = (value: unknown) => {
+    try {
+      window.sessionStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log('setItem 오류', error);
+    }
+  };
+
+  const getItem = () => {
+    try {
+      const item = window.sessionStorage.getItem(key);
+      return item ? JSON.parse(item) : null;
+    } catch (error) {
+      console.log('getItem 오류', error);
+    }
+  };
+
+  const removeItem = () => {
+    try {
+      window.sessionStorage.removeItem(key);
+    } catch (error) {
+      console.log('removeItem 오류', error);
+    }
+  };
+
+  return { setItem, getItem, removeItem };
+};
