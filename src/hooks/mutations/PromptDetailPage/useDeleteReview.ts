7199f79acb6645aa6525902c7bcb9767a462a862
fbehdgus <danny90697@gmail.com>
2025-11-16 02:13:04 +0900

    ♻️ Refactor: 리뷰삭제 훅에 업데이트 쿼리키 추가(#307)

--- src/hooks/mutations/PromptDetailPage/useDeleteReview.ts
+++ src/hooks/mutations/PromptDetailPage/useDeleteReview.ts
@@ -1,8 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteReview } from '@/apis/PromptDetailPage/reviews';
 
 export default function useDeleteReview() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (reviewId: number) => deleteReview(reviewId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['reviews'] });
+      queryClient.invalidateQueries({ queryKey: ['myDownloadedPrompts'] });
+    },
   });
 }
