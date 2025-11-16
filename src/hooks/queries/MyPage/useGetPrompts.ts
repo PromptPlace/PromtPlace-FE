fcef1aefa982b74cf75e11ed580cd6e7189d730d
fbehdgus <danny90697@gmail.com>
2025-11-16 20:38:49 +0900

    ♻️ Refactor: 찜한 프롬프트 훅 수정(#307)

--- src/hooks/queries/MyPage/useGetPrompts.ts
+++ src/hooks/queries/MyPage/useGetPrompts.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import type { UseQueryOptions } from '@tanstack/react-query';
 import { getDownloadedPrompts, getLikedPrompts } from '@/apis/MyPage/prompt.ts';
-import type { ApiResponse, DownloadedPromptDTO, LikedPromptDTO } from '@/types/MyPage/prompt.ts';
+import type { ApiResponse, DownloadedPromptDTO, LikedPromptDTO,NewLikedPromptDTO } from '@/types/MyPage/prompt.ts';
 import type { AxiosError } from 'axios';
 
 // 쿼리 키를 상수로 관리하면 오타를 방지하고 일관성을 유지할 수 있습니다.
@@ -25,10 +25,9 @@ export const useGetDownloadedPrompts = (options?: UseGetDownloadedPromptsOptions
   });
 };
 
-export const useGetLikedPrompts = (options?: UseGetLikedPromptsOptions) => {
-  return useQuery<ApiResponse<LikedPromptDTO>, AxiosError>({
+export const useGetLikedPrompts = () => {
+  return useQuery({
     queryKey: QUERY_KEY.likedPrompts,
     queryFn: getLikedPrompts,
-    ...options,
   });
 };
