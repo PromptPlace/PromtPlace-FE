05a6970631ce14a084c343901dbd21fd7ecb46c9
jinhyo <kjhyo0220@gmail.com>
2025-12-29 18:21:32 +0900

    ✨ Feat: 운영자 메시지 모달 퍼블리싱 (#357)

---
+++ src/components/Modal/AdminModal.tsx
@@ -0,0 +1,45 @@
+import DefaultImg from '@assets/logo/app/app-logo-default.svg?react';
+
+const AdminModal = () => {
+  return (
+    <div className="fixed inset-0 bg-overlay flex justify-center items-center px-[102px] max-lg:p-[40px] max-phone:p-[20px]">
+      <div className="max-w-[825px] w-full bg-white rounded-[16px] flex flex-col gap-[16px] p-[32px]">
+        <div className="text-center custom-h4 pb-[16px] border-b border-b-gray200">운영자 메시지</div>
+
+        <div className="flex items-start gap-[24px]">
+          <DefaultImg className="w-[48px] h-[48px]" />
+
+          <div className="flex flex-col gap-[20px] custom-body2 pt-[8px] pr-[20px] max-h-[412px] h-full overflow-y-auto">
+            <p className="text-text-on-white">운영자에게 메시지가 도착했어요!</p>
+
+            <div className="text-gray700">
+              안녕하세요, 주연님. 프롬프트 플레이스 운영팀입니다.
+              <br />
+              <br />
+              주연님께서 지난 11월 9일에 업로드하신 {`<`}발표의 신: 청중을 사로잡는 대본 & 날카로운 예상 질문 방어{`>`}{' '}
+              프롬프트와 관련하여 안내 말씀드립니다.
+              <br />
+              <br />
+              현재 해당 콘텐츠의 상세 설명이 다소 부족한 것으로 확인되어, 이용자들의 이해를 돕기 위해 내용 보완을
+              요청드립니다.
+              <br />
+              <br />
+              번거로우시겠지만 7일 이내에 내용을 수정해 주시기를 부탁드립니다. 기한 내에 수정이 이루어지지 않을 경우,
+              부득이하게 게시 중단(삭제) 조치가 진행될 수 있는 점 너른 양해 부탁드립니다.
+              <br />
+              <br />
+              감사합니다.
+              <br />
+              <br />
+              프롬프트 플레이스 드림
+            </div>
+
+            <p className="text-gray400">2025.10.18</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminModal;
--- src/components/Navbar/NavbarNotificatioinModalCard.tsx
+++ src/components/Navbar/NavbarNotificatioinModalCard.tsx
@@ -1,6 +1,8 @@
 import DefaultImg from '@assets/logo/app/app-logo-default.svg?react';
 import { useNavigate } from 'react-router-dom';
 import PrimaryButton from '../Button/PrimaryButton';
+import { useState } from 'react';
+import AdminModal from '../Modal/AdminModal';
 
 interface NavbarNotificatioinModalCardProps {
   img: string | null;
@@ -19,44 +21,52 @@ const NavbarNotificatioinModalCard = ({
   type,
   setIsNotificationModalShow,
 }: NavbarNotificatioinModalCardProps) => {
+  const [showAdminModal, setShowAdminModal] = useState<boolean>(false);
+
   const navigate = useNavigate();
 
   return (
-    <div className="pt-[8px] pr-[24px] pb-[20px]">
-      <div
-        onClick={() => {
-          setIsNotificationModalShow((prev) => !prev);
-          navigate(link);
-        }}
-        className="flex gap-[24px]">
-        {img ? (
-          <img src={img} className="w-[48px] h-[48px] rounded-full object-cover" />
-        ) : (
-          <DefaultImg className="w-[48px] h-[48px]" />
-        )}
+    <>
+      <div className="pt-[8px] pr-[24px] pb-[20px]">
+        <div
+          onClick={() => {
+            setIsNotificationModalShow((prev) => !prev);
+            navigate(link);
+          }}
+          className="flex gap-[24px]">
+          {img ? (
+            <img src={img} className="w-[48px] h-[48px] rounded-full object-cover" />
+          ) : (
+            <DefaultImg className="w-[48px] h-[48px]" />
+          )}
 
-        <div className="flex flex-col gap-[8px]">
-          <p className="custom-body2">{content}</p>
-          <p className="custom-body3 text-gray400">{date.slice(0, 10)}</p>
+          <div className="flex flex-col gap-[8px]">
+            <p className="custom-body2">{content}</p>
+            <p className="custom-body3 text-gray400">{date.slice(0, 10)}</p>
+          </div>
         </div>
+
+        {type === 'ADMIN' && (
+          <div className="flex justify-end">
+            <PrimaryButton
+              buttonType="square"
+              text="확인하기"
+              py={6}
+              px={12}
+              textSize={12}
+              textColor="white"
+              borderRadius={8}
+              width={47}
+              onClick={() => {
+                setShowAdminModal(true);
+              }}
+            />
+          </div>
+        )}
       </div>
 
-      {type === 'ADMIN' && (
-        <div className="flex justify-end">
-          <PrimaryButton
-            buttonType="square"
-            text="확인하기"
-            py={6}
-            px={12}
-            textSize={12}
-            textColor="white"
-            borderRadius={8}
-            width={47}
-            onClick={() => {}}
-          />
-        </div>
-      )}
-    </div>
+      {showAdminModal && <AdminModal />}
+    </>
   );
 };
 
