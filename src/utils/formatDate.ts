adac1ed9fada709ff7357c883255e6b6487f9423
jinhyo <kjhyo0220@gmail.com>
2026-01-22 15:06:26 +0900

    🐛 Fix: formatDate utils 함수 수정 (#372)
    년도, 요일까지 반환하도록

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,10 +1,14 @@
+const DAYS = ['일', '월', '화', '수', '목', '금', '토'];
+
 function formatDate(date: Date | string) {
   const parsedDate = date instanceof Date ? date : new Date(date); // Date 객체 생성
 
+  const year = parsedDate.getFullYear(); //년
   const month = parsedDate.getMonth() + 1; // 월
   const day = parsedDate.getDate(); // 일
+  const dayOfWeek = DAYS[parsedDate.getDay()]; // 요일
 
-  return `${month}월 ${day}일`;
+  return { year, month, day, dayOfWeek };
 }
 
 export default formatDate;
