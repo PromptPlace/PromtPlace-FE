7bcb26ca7c3d3060fced60e3a800e6ab88c6f2de
jinhyo <kjhyo0220@gmail.com>
2026-02-16 15:27:50 +0900

    ✨ Feat: 채팅방 상세 조회 api 연결 (#372)

---
+++ src/hooks/queries/ChatPage/useGetChatRoomsDetail.ts
@@ -0,0 +1,19 @@
+import { getChatRoomsDetail } from '@/apis/ChatPage/chat';
+import { QUERY_KEY } from '@/constants/key';
+import type { ResponseChatRoomsDetailDto } from '@/types/ChatPage/chat';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+function useGetChatRoomsDetail(roomId: number, limit?: number) {
+  return useInfiniteQuery<ResponseChatRoomsDetailDto>({
+    queryKey: [QUERY_KEY.chatRooms, roomId, limit],
+    queryFn: ({ pageParam = 0 }) => getChatRoomsDetail(roomId, { cursor: pageParam as number, limit }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      if (!lastPage.data.page.has_more) return undefined;
+
+      return lastPage.data.messages.at(-1)?.message_id ?? undefined;
+    },
+  });
+}
+
+export default useGetChatRoomsDetail;
--- src/types/ChatPage/pagination.ts
+++ src/types/ChatPage/pagination.ts
@@ -2,3 +2,8 @@ export type PaginationDto = {
   cursor?: number;
   limit?: number;
 };
+
+export type Page = {
+  has_more: boolean;
+  total_count: number;
+};
