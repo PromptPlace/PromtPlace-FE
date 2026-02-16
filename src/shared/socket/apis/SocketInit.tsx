4b4d6be0102caa0f4a629d5fe131faf4fd8ef7a0
jinhyo <kjhyo0220@gmail.com>
2026-02-16 13:46:07 +0900

    ✨ Feat: 웹소켓 연결 (#372)

---
+++ src/shared/socket/apis/SocketInit.tsx
@@ -0,0 +1,21 @@
+import { useAuth } from '@/context/AuthContext';
+import { useEffect } from 'react';
+import { disconnectSocket, initSocket } from './socket';
+
+const SocketInit = () => {
+  const { accessToken } = useAuth();
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    initSocket(accessToken);
+
+    return () => {
+      disconnectSocket();
+    };
+  }, [accessToken]);
+
+  return null;
+};
+
+export default SocketInit;
