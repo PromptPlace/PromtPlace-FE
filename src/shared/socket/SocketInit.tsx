09daa22c8128dda1fbb5470fcf96641e34969964
jinhyo <kjhyo0220@gmail.com>
2026-02-16 14:04:59 +0900

    🚚 Mod: 파일위치 변경 (#372)

--- src/shared/socket/SocketInit.tsx
+++ src/shared/socket/SocketInit.tsx
@@ -1,6 +1,6 @@
 import { useAuth } from '@/context/AuthContext';
 import { useEffect } from 'react';
-import { disconnectSocket, initSocket } from './socket';
+import { disconnectSocket, initSocket } from './apis/socket';
 
 const SocketInit = () => {
   const { accessToken } = useAuth();
