4e0dd0c43666fdb4342ebff8424f07bd04c4e0e5
fbehdgus <danny90697@gmail.com>
2026-03-14 02:31:11 +0900

    ✨ Feat: 정산관리 탭 관련 타입 정의(#390)

---
+++ src/types/MyPage/sellerInfo.ts
@@ -0,0 +1,38 @@
+/**
+ * 판매자 정보 수정 관련 타입 정의
+ */
+
+export type SellerType = 'individual' | 'business';
+
+export interface SellerInfoEditData {
+  sellerType: SellerType;
+  name?: string;
+  businessNumber?: string;
+  representativeName?: string;
+  companyName?: string;
+  businessLicenseUrl?: string;
+  bank: string;
+  accountNumber: string;
+  holderName: string;
+  isTermsAgreed: boolean;
+  accountVerified: boolean;
+}
+
+export interface SellerInfoEditFormData {
+  sellerType: SellerType;
+  name?: string;
+  businessNumber?: string;
+  representativeName?: string;
+  companyName?: string;
+  businessLicenseUrl?: string;
+  bank: string;
+  accountNumber: string;
+  holderName: string;
+  isTermsAgreed: boolean;
+}
+
+export interface SellerInfoEditResponse {
+  message: string;
+  data: SellerInfoEditData;
+  statusCode: number;
+}
