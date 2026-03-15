16df82833ce1879fca54ca544a33feb2132131b5
jinhyo <kjhyo0220@gmail.com>
2026-03-15 11:48:20 +0900

    ✨ Feat: 상대방 차단 (#372)

--- src/pages/ChatPage/components/ChatMenu.tsx
+++ src/pages/ChatPage/components/ChatMenu.tsx
@@ -1,7 +1,9 @@
 import usePatchLeaveChat from '@/hooks/mutations/ChatPage/usePatchLeaveChat';
+import usePostBlockChat from '@/hooks/mutations/ChatPage/usePostBlockChat';
 
 interface ChatMenuProps {
   roomId: number;
+  blocked_user_id: number;
 }
 
 const MENUS = [
@@ -9,8 +11,9 @@ const MENUS = [
   { id: 2, label: '메시지 차단' },
 ];
 
-const ChatMenu = ({ roomId }: ChatMenuProps) => {
+const ChatMenu = ({ roomId, blocked_user_id }: ChatMenuProps) => {
   const { mutate: mutatePatchLeaveChat } = usePatchLeaveChat(); // 채팅방 나가기
+  const { mutate: mutatePostBlockChat } = usePostBlockChat(); // 상대방 차단
 
   return (
     <div className="w-[142px] h-[94px] p-[10px] rounded-[8px] border border-white-stroke shadow-md flex flex-col items-center justify-center">
@@ -19,6 +22,7 @@ const ChatMenu = ({ roomId }: ChatMenuProps) => {
           key={menu.id}
           onClick={() => {
             if (menu.id === 1) mutatePatchLeaveChat(roomId);
+            else mutatePostBlockChat(blocked_user_id);
           }}
           className="custom-body3 text-text-on-background p-[10px] cursor-pointer hover:text-gray600 duration-100">
           {menu.label}
